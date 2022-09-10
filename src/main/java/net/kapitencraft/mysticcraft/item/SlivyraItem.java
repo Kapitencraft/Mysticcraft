ebf259e42c002b7a3096fac699a497473c940636
Kapitencraft <kapitencraft@gmail.com>
2022-09-10 18:34:38 +0200

    Deleted Staff of the wild;

--- src/main/java/net/kapitencraft/mysticcraft/item/SlivyraItem.java
+++ src/main/java/net/kapitencraft/mysticcraft/item/SlivyraItem.java
@@ -1,33 +1,18 @@
 package net.kapitencraft.mysticcraft.item;
 
 import net.kapitencraft.mysticcraft.item.spells.SpellItem;
-import net.kapitencraft.mysticcraft.misc.MISCTools;
 import net.kapitencraft.mysticcraft.spell.SpellSlot;
 import net.kapitencraft.mysticcraft.spell.Spells;
-import net.kapitencraft.mysticcraft.spell.spells.CrystalWarpSpell;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
-
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.stream.Collectors;
 
 public class SlivyraItem extends SpellItem {
 
     public final int spellslotamount = 1;
-    private int activespell;
     SpellSlot[] spellSlots = new SpellSlot[spellslotamount];
 
     public SlivyraItem(Properties p_43272_) {
         super(p_43272_);
-        this.spellSlots[1] = new SpellSlot(Spells.CRYSTALWARP, null);
+        this.spellSlots[0] = new SpellSlot(Spells.CRYSTALWARP, null);
     }
 
 
--- src/main/java/net/kapitencraft/mysticcraft/spell/SpellSlot.java
+++ src/main/java/net/kapitencraft/mysticcraft/spell/SpellSlot.java
@@ -5,23 +5,16 @@ import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 
 public class SpellSlot {
     private Spell spell;
-    private SpellModdifier moddifier;
     private double mana_cost;
 
     public SpellSlot(Spell spell, SpellModdifier moddifier) {
         this.spell = spell;
-        this.moddifier = moddifier;
         this.calculateManaCost();
     }
 
     public Spell getSpell() {
         return this.spell;
     }
-
-    public SpellModdifier getModdifier() {
-        return this.moddifier;
-    }
-
     public double getManaCost() {
         return this.mana_cost;
     }
@@ -31,14 +24,7 @@ public class SpellSlot {
     }
 
     public double calculateManaCost() {
-        double Mana_Cost = (double) this.spell.MANA_COST;
-        double amount = this.moddifier.getModifier().get(ModAttributes.MANA_COST.get()).getAmount();
-        if (this.moddifier.getModifier().get(ModAttributes.MANA_COST.get()).getOperation() == AttributeModifier.Operation.ADDITION) {
-            Mana_Cost += amount;
-        } else if (this.moddifier.getModifier().get(ModAttributes.MANA_COST.get()).getOperation() == AttributeModifier.Operation.MULTIPLY_TOTAL) {
-            Mana_Cost *= amount;
-        }
-        this.mana_cost = Mana_Cost;
-        return Mana_Cost;
+        this.mana_cost = this.spell.MANA_COST;
+        return this.spell.MANA_COST;
     }
 }
