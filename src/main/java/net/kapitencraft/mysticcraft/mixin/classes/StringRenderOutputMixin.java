6003bb0999eb337dca2ee5a275f91593b87f62c8
Kapitencraft <kapitencraft@gmail.com>
2023-11-23 21:03:16 +0100

    oh added ATs

---
+++ src/main/java/net/kapitencraft/mysticcraft/mixin/classes/StringRenderOutputMixin.java
@@ -0,0 +1,10 @@
+package net.kapitencraft.mysticcraft.mixin.classes;
+
+
+import net.minecraft.client.gui.Font;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Font.StringRenderOutput.class)
+public class StringRenderOutputMixin {
+
+}
