ca39672ba599645f800bb5b899f2bf81e11510fe
Kapitencraft <kapitencraft@gmail.com>
2024-01-30 21:08:51 +0100

    fixed display not working for new Requirements;

--- src/main/java/net/kapitencraft/mysticcraft/init/ModRequirements.java
+++ src/main/java/net/kapitencraft/mysticcraft/init/ModRequirements.java
@@ -3,6 +3,7 @@ package net.kapitencraft.mysticcraft.init;
 import net.kapitencraft.mysticcraft.MysticcraftMod;
 import net.kapitencraft.mysticcraft.init.custom.ModRegistryKeys;
 import net.kapitencraft.mysticcraft.requirements.Requirement;
+import net.kapitencraft.mysticcraft.requirements.type.CustomStatReqType;
 import net.kapitencraft.mysticcraft.requirements.type.StatReqType;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.entity.EntityType;
@@ -16,8 +17,8 @@ public interface ModRequirements {
     DeferredRegister<Requirement<?>> REGISTRY = MysticcraftMod.makeCustomRegistry(ModRegistryKeys.REQUIREMENTS, RegistryBuilder::new);
 
     RegistryObject<Requirement<Item>> ELYTRA_REQ = REGISTRY.register("elytra_req", ()-> new Requirement<>(new StatReqType(Stats.ENTITY_KILLED.get(EntityType.ENDER_DRAGON), 5), ()-> Items.ELYTRA));
-    RegistryObject<Requirement<Item>> HYPERION_REQ = REGISTRY.register("hyperion_req", ()-> new Requirement<>(new StatReqType(Stats.CUSTOM.get(ModStatTypes.STORMS_KILLED.get()), 10), ModItems.HYPERION));
-    RegistryObject<Requirement<Item>> VALKYRIE_REQ = REGISTRY.register("valkyrie_req", ()-> new Requirement<>(new StatReqType(Stats.CUSTOM.get(ModStatTypes.NECRONS_KILLED.get()), 10), ModItems.VALKYRIE));
-    RegistryObject<Requirement<Item>> SCYLLA_REQ = REGISTRY.register("scylla_req", ()-> new Requirement<>(new StatReqType(Stats.CUSTOM.get(ModStatTypes.MAXORS_KILLED.get()), 10), ModItems.SCYLLA));
-    RegistryObject<Requirement<Item>> ASTREA_REQ = REGISTRY.register("astrea_req", ()-> new Requirement<>(new StatReqType(Stats.CUSTOM.get(ModStatTypes.GOLDORS_KILLED.get()), 10), ModItems.ASTREA));
+    RegistryObject<Requirement<Item>> HYPERION_REQ = REGISTRY.register("hyperion_req", ()-> new Requirement<>(new CustomStatReqType(Stats.CUSTOM.get(ModStatTypes.STORMS_KILLED.get()), 10, "stat_req.storm"), ModItems.HYPERION));
+    RegistryObject<Requirement<Item>> VALKYRIE_REQ = REGISTRY.register("valkyrie_req", ()-> new Requirement<>(new CustomStatReqType(Stats.CUSTOM.get(ModStatTypes.NECRONS_KILLED.get()), 10, "stat_req.necron"), ModItems.VALKYRIE));
+    RegistryObject<Requirement<Item>> SCYLLA_REQ = REGISTRY.register("scylla_req", ()-> new Requirement<>(new CustomStatReqType(Stats.CUSTOM.get(ModStatTypes.MAXORS_KILLED.get()), 10, "stat_req.maxor"), ModItems.SCYLLA));
+    RegistryObject<Requirement<Item>> ASTREA_REQ = REGISTRY.register("astrea_req", ()-> new Requirement<>(new CustomStatReqType(Stats.CUSTOM.get(ModStatTypes.GOLDORS_KILLED.get()), 10, "stat_req.goldor"), ModItems.ASTREA));
 }
---
+++ src/main/java/net/kapitencraft/mysticcraft/requirements/type/CustomStatReqType.java
@@ -0,0 +1,24 @@
+package net.kapitencraft.mysticcraft.requirements.type;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.stats.Stat;
+
+import java.util.function.Function;
+
+public class CustomStatReqType extends StatReqType {
+    private final Function<Integer, Component> display;
+    public CustomStatReqType(Stat<ResourceLocation> stat, int level, Function<Integer, Component> display) {
+        super(stat, level);
+        this.display = display;
+    }
+
+    public CustomStatReqType(Stat<ResourceLocation> stat, int level, String translateKey) {
+        this(stat, level, integer -> Component.translatable(translateKey, integer));
+    }
+
+    @Override
+    public Component display() {
+        return display.apply(minLevel);
+    }
+}
